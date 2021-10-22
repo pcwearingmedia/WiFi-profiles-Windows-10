77e55844500ef746178d66f090e1cc5619b23e53
P-C Markovski <pcwriter@wearingmedia.com>
2021-10-22 12:54:48 +0200

    wifi.bat updated.
    Comment added with example on how to retrieve the key for WiFi SSID.

--- wifi.bat
+++ wifi.bat
@@ -8,6 +8,7 @@ Rem Show the WiFi profiles.
 netsh wlan show profiles > wifi_stored.txt
 
 Rem Show complete information about WiFi device and stored network profiles.
+:: To extract the WiFi key, use the following construct (per network): netsh wlan show profiles <SSID name> key=clear
 netsh wlan show all >> wifi_stored.txt
 
 Rem Display the current WiFi network.
